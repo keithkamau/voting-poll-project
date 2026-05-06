1d761dd3d54a098c52dc43fdf770809dbf4a817b
Keith Kamau <keith.kamau2@student.moringaschool.com>
2026-05-06 22:12:10 +0300

    Login and Register component linking

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "./index.css";
 const App = lazy(() => import("./App.jsx"));
 const About = lazy(() => import("./About.jsx"));
 const Login = lazy(() => import("./components/auth/login/index.jsx"));
-const Register = lazy(() => import("./components/auth/register.index.jsx"));
+const Register = lazy(() => import("./components/auth/register/index.jsx"));
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
