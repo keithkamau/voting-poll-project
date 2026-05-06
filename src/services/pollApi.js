822c6cc47edf287e338b46e71038f5dd6aafbb77
Emmanuel Munene <emmanuel.munene@student.moringaschool.com>
2026-05-06 17:05:30 +0300

    point pollApi to Render backend

--- src/services/pollApi.js
+++ src/services/pollApi.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3001";
+const API_URL = "https://voting-poll-backend-qzou.onrender.com";
 
 const request = async (path, options = {}) => {
   const response = await fetch(`${API_URL}${path}`, {
